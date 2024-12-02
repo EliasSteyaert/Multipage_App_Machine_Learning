70fb53b6a8f411a5c572725cddf394ec8d609bcb
Elias Steyaert <elias.steyaert@student.howest.be>
2024-12-02 09:29:15 +0100

    making a file with the css/hmtl code for the toolbox

---
+++ styles.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+def load_css():
+    st.markdown("""
+    <style>
+    /* Style for the tooltip icon and the question mark emoji */
+    .tooltip {
+        position: relative;
+        display: inline-block;
+        cursor: pointer;
+        color: inherit;  /* Inherit text color from parent */
+        font-size: inherit;  /* Inherit font size */
+        text-decoration: none;  /* Remove underline */
+    }
+    
+    .emoji {
+        font-size: 1.2em;  /* Slightly larger size for the emoji */
+        margin-left: 5px;  /* Space between the text and the emoji */
+    }
+
+    .tooltip .tooltiptext {
+        visibility: hidden;
+        width: 300px;
+        background-color: #f9f9f9;
+        color: #333;
+        text-align: center;
+        border-radius: 5px;
+        padding: 5px;
+        position: absolute;
+        z-index: 1;
+        bottom: 125%;
+        left: 50%;
+        margin-left: -150px;
+        opacity: 0;
+        transition: opacity 0.3s;
+    }
+
+       /* Adjust margin for markdown (the title or explanation above the slider) */
+    .stMarkdown {
+        margin-bottom: -10px;  /* Adjust the space between the title and slider */
+        margin-top: -10px;
+    }
+    .stSlider {
+        margin-top: -20px;  /* Reduce the space above the slider */
+    }
+    .tooltip:hover .tooltiptext {
+        visibility: visible;
+        opacity: 1;
+    }
+    </style>
+    """, unsafe_allow_html=True)
